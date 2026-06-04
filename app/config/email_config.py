30ed894c2d9c9fe49c991021d2feb281084271a0
matheesha27 <matheesha27@gmail.com>
2026-06-05 01:14:14 +0530

    Configure Brevo SMTP

--- app/config/email_config.py
+++ app/config/email_config.py
@@ -1,13 +1,18 @@
+import os
 from fastapi_mail import ConnectionConfig
 
 conf = ConnectionConfig(
+    MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
+    MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
+    MAIL_FROM=os.getenv("MAIL_FROM"),
+    MAIL_FROM_NAME="Movie Ticket Booking",
 
-    MAIL_USERNAME = "matheesha27@gmail.com",
-    MAIL_PASSWORD = "lmpfhwkcqtpjyeqr",
-    MAIL_FROM = "matheesha27@gmail.com",
-    MAIL_PORT = 587,
-    MAIL_SERVER = "smtp.gmail.com",
-    MAIL_STARTTLS = True,
-    MAIL_SSL_TLS = False,
-    USE_CREDENTIALS = True
+    # 💡 CRITICAL BREVO AND PORT 2525 ROUTING CONFIGURATION:
+    MAIL_SERVER="smtp-relay.brevo.com",
+    MAIL_PORT=2525,  # Bypasses the Render port 587 block completely
+    MAIL_STARTTLS=True,  # Must be True for port 2525
+    MAIL_SSL_TLS=False,  # Must be False for port 2525
+
+    USE_CREDENTIALS=True,
+    VALIDATE_CERTS=True
 )
